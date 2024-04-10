b5ff99b72bd0ac60b103425c005d147e826e3c34
Parth2007a <111279382+Parth2007a@users.noreply.github.com>
2024-04-10 17:50:43 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Video Calling Web App</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script src='https://cdn.scaledrone.com/scaledrone.min.js'></script>
+	<script type="text/javascript" src="script.js"></script>
+</head>
+<body>
+	<div class="header">
+		<h1 align="center">Video Calling Web App</h1>
+	</div>
+
+	<div class="content">
+		<video class="from" id="localVideo" autoplay controls></video> 
+		<video class="to" id="remoteVideo" autoplay controls></video> 
+	</div>
+
+	<div class="footer">
+		<h1 align="center">@copyright</h1>
+	</div>
+</body>
+</html>
